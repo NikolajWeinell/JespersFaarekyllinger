085761a1b7da8525e21b57eed75367b03f482d2f
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-24 15:55:20 +0100

    Task 1 completed

--- Week 2/Task 1 - functions/Main.java
+++ Week 2/Task 1 - functions/Main.java
@@ -1,20 +1,25 @@
 
 public class Main {
+    public static void main(String[] args) {
+        
+        stringInput("Test");
+        printLine();
+        nameage("Nikolaj", 25);
+
+    }
 
     public static void printLine()  {
 
         System.out.println("");
+    }
+    
+    public static void stringInput(String param)    {
 
+        System.out.println(param);
+    }
 
-    public static void main(String[] args) {
-        
-        }
+    public static void nameage(String name, int age) {
 
-    printLine();
-
-
-
-
-
+        System.out.println("My name is " + name + ", I am " + age + " years old");
     }
 }
