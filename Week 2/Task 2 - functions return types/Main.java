77535c30c9dce05f1cc11e3ad26f143d40e05f63
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-24 16:31:49 +0100

    Task 2
    2.a????

---
+++ Week 2/Task 2 - functions return types/Main.java
@@ -0,0 +1,51 @@
+
+class Main{
+  public static boolean happy = true;
+
+
+public static void main(String [] args) {
+   if (iAmHappy())
+   {
+     System.out.println("I clap my hands");
+   }
+   else
+   {
+     System.out.println("I don't clap my hands"); 
+   }
+
+System.out.println(intSum(5,7));
+System.out.println(upperCase("test"));
+
+}
+
+
+public static boolean iAmHappy()
+{
+  // fill out what is missing here: 
+
+
+  return true;
+}
+
+
+public static int intSum(int int1, int int2) {
+
+    return int1 + int2;
+}
+
+public static String upperCase(String str)  {
+
+    str = str.toUpperCase();
+
+    return str;
+}
+
+public static String isUpperCase(String str)    {
+
+
+
+    return str;
+}
+
+
+}
