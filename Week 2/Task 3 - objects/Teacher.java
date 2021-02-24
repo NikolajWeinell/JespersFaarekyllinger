daf92c918554aa849e9f87fbee5affc9d0ef785c
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-24 17:17:24 +0100

    Update Teacher.java

---
+++ Week 2/Task 3 - objects/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    
+    private String name;
+    private int age;
+    private boolean isFemale;
+    
+    public isTeacher(String tmpName, int tmpAge, boolean tmpIsFemale)   {
+
+        this.name = tmpName;
+        this.age = tmpAge;
+        this.isFemale = tmpIsFemale;
+
+    }
+
+}
