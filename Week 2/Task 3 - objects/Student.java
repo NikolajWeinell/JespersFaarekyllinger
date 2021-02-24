bf86413b3914f8b70b02ab2ac32e8274403c6ac2
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-24 17:16:56 +0100

    Update Student.java

---
+++ Week 2/Task 3 - objects/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+
+
+    private String name;
+    private int age;
+    private boolean isFemale;
+    private boolean datamatikerTeam;
+
+    public isStudent(String tmpName, int tmpAge, boolean tmpIsFemale, boolean tmpDatamatikerTeam)    {
+
+        this.name = tmpName;
+        this.age = tmpAge;
+        this.isFemale = tmpIsFemale;
+        this.datamatikerTeam = tmpDatamatikerTeam;
+        
+
+    }
+
+    
+}
