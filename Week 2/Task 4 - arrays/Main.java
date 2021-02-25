200ae7c1704444fa4306dbb328c79844a6be1ee2
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-25 14:11:46 +0100

    Task 4 monkaS

--- Week 2/Task 4 - arrays/Main.java
+++ Week 2/Task 4 - arrays/Main.java
@@ -1,16 +1,32 @@
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println(intarray);
+        int[] intarray = {2,4,6,8};
+        String[] stringarray;
+        boolean[] booleanarray = {true, false, false};
 
+       
+        stringarrayM();
+
+        for (int tal: intarray)  {
+            int sum = tal + tal;
+            System.out.println(sum);
+        }
+
+        for (boolean bool: booleanarray)   {
+            System.out.println(bool);
+        }
     }
 
+    public static void stringarrayM()   {
 
-    int[] intarray;
-    String[] stringarray;
-    boolean[] booleanarray;
+        String[] stringarray = {"Datamatiker", "POGGERS", "KomodoHype"};
 
+        for (String str: stringarray)   {
+            System.out.println(str);
+        }
 
-
-
+    }
 }
+
+
