aa84a38914c464370c901dca023bec73effa5d15
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-25 15:12:17 +0100

    Task 6 not completed :(

---
+++ Week 2/Task 6/debug.java
@@ -0,0 +1,35 @@
+public class debug {
+    
+/*
+ *
+ * Hvis du kompilerer programmet får du 4 fejl.
+ * Kan du fixe programmet så det kan køre og
+ * outputtet bliver:
+ * "7 is the smallest!"
+ *
+ * 
+ */
+
+    
+public static void main(String[] args) {
+    public class Oops {
+        int a = 7;
+        int b = 42;
+        minimum(a,b);
+        if (smaller == a) {
+        System.out.println("a is the smallest!");
+          }
+        }
+        public static void minimum(int a, int b) {
+          if (a < b) {
+            int smaller = a;
+          } else {
+            int smaller = b;
+          }
+        }
+          return int smaller;
+        
+    }
+}
+}
+
