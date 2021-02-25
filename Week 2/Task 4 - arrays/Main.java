c9ad765d6ea1679c1f1e735ae945252a348a5006
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-25 15:12:07 +0100

    Task 4 not completed :(

--- Week 2/Task 4 - arrays/Main.java
+++ Week 2/Task 4 - arrays/Main.java
@@ -12,38 +12,64 @@ public class Main {
         for (boolean bool: booleanarray)   {
         System.out.println(bool);
         }
+
+    summer();
+
+    }   
+}
+
+
+public static void stringarrayM()   {
+
+    String[] stringarray = {"Datamatiker", "POGGERS", "KomodoHype"};
+
+    for (String str: stringarray)   {
+        System.out.println(str);
     }
 
-    public int summer(int sum)  {
+}
+
+public int summer(int sum)  {
         
-        for (int tal: intarray)  {
-            sum += tal;
-            return sum;
-        }
+    for (int tal: intarray)  {
+        sum += tal;
+        return sum;
     }
+}
 
-    public int averager(int avg)  {
-        
-        for (int tal: intarray)  {
-            sum += tal;
-            int len = intarray.length;
-            avg = sum/len;
-            return avg;
-        }
-    }
-
-    public static void stringarrayM()   {
-
-        String[] stringarray = {"Datamatiker", "POGGERS", "KomodoHype"};
-
-        for (String str: stringarray)   {
-            System.out.println(str);
-        }
-
+public int averager(int avg)  {
+    
+    for (int tal: intarray)  {
+        sum += tal;
+        int len = intarray.length;
+        avg = sum/len;
+        return avg;
     }
 }
 
 
+
+
+public static void sortArr(int[] inputArr)
+    {
+        int tmp = 0;
+
+        for(int i = 0; i < inputArr.length; i++)
+        {
+            for (int j = i + 1; j < inputArr.length; j++)
+            {
+                
+                if (inputArr[i] > inputArr[j])
+                {
+                    tmp = inputArr[i];
+                    inputArr[i] = inputArr[j];
+                    inputArr[j] = tmp;
+                }
+            }
+            System.out.println(inputArr[i]);
+        }
+
+    }
 
 
 
