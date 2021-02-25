11aa74539289d718ff6e4daaa0c7c1c7f2a138f7
NikolajWeinell <58971957+NikolajWeinell@users.noreply.github.com>
2021-02-25 11:25:32 +0100

    Task 4 created

---
+++ Week 2/Task 4 - arrays/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println(intarray);
+
+    }
+
+
+    int[] intarray;
+    String[] stringarray;
+    boolean[] booleanarray;
+
+
+
+
+}
